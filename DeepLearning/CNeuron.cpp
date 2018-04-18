5c9a7da26c8bb565939f68ee3b2420a97d19d6aa
kusa-mochi <whoatemyapplepie@gmail.com>
2018-04-18 22:52:50 +0900

    Add Eigen Library.

---
+++ DeepLearning/CNeuron.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "CNeuron.h"
+
+
+CNeuron::CNeuron()
+{
+}
+
+
+CNeuron::~CNeuron()
+{
+}
---
+++ DeepLearning/CNeuron.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "stdafx.h"
+
+public class CNeuron
+{
+public:
+	CNeuron();
+	virtual ~CNeuron();
+};
