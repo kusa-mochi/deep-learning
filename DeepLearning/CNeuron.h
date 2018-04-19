38ccbfdcbf76a841f78a5960723924383475d6f4
kusa-mochi <whoatemyapplepie@gmail.com>
2018-04-19 22:47:23 +0900

    no message

--- DeepLearning/CNeuron.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "stdafx.h"
-
-public class CNeuron
-{
-public:
-	CNeuron();
-	virtual ~CNeuron();
-};
---
+++ DeepLearning/MultilayerPerceptron.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "MultilayerPerceptron.h"
+
+
+MultilayerPerceptron::MultilayerPerceptron(
+	int numLayer,
+	int* numNeuron,
+	double*** weight,
+	double (*ActivationFunction)(double* d),
+	double (*OutputActivationFunction)(double* d)
+)
+{
+}
+
+
+MultilayerPerceptron::~MultilayerPerceptron()
+{
+}
