9e4cb6a62810dad80bef5d502ad96eadfe2a5d6d
kusa-mochi <whoatemyapplepie@gmail.com>
2018-05-04 23:58:52 +0900

    add assertion.

--- DeepLearningCore/AffineLayerCore.cpp
+++ DeepLearningCore/AffineLayerCore.cpp
@@ -6,6 +6,13 @@ namespace DeepLearningCore
 {
 	AffineLayerCore::AffineLayerCore(MatrixXX* pW, VectorXX* pB)
 	{
+		assert((*pW).rows() > 0);
+		assert((*pW).cols() > 0);
+		assert((*pB).rows() > 0);
+		assert((*pB).cols() > 0);
+		assert((*pW).cols() == (*pB).cols());
+		assert((*pB).rows() == 1);
+
 		_pW = pW;
 		_pB = pB;
 	}
@@ -18,6 +25,10 @@ namespace DeepLearningCore
 
 	MatrixXX AffineLayerCore::Forward(MatrixXX x)
 	{
+		assert(x.rows() > 0);
+		assert(x.cols() > 0);
+		assert(x.cols() == (*_pW).rows());
+
 		_x = x;
 
 		MatrixXX output = x * (*_pW);
@@ -41,10 +52,18 @@ namespace DeepLearningCore
 
 	LayerBackwardOutput AffineLayerCore::Backward(MatrixXX dout)
 	{
+		assert(dout.rows() > 0);
+		assert(dout.cols() > 0);
+		assert(dout.cols() == (*_pW).cols());
+		assert(_x.rows() == dout.rows());
+
 		LayerBackwardOutput output;
 		output.x = dout * _pW->transpose();
 		_dw = _x.transpose() * dout;
 		_db = dout.colwise().sum();
+
+		assert(_db.rows() == 1);
+		assert(_db.cols() == dout.cols());
 
 		return output;
 	}
